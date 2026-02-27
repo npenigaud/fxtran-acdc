f044e7a65bbea27e261bfb218a9a96ee40551b08
penigaudn <nicolas.penigaud@meteo.fr>
2026-02-27 15:37:06 +0100

    changes to use rocprofv3 and roctx

---
+++ tests/prof_test.sh
@@ -0,0 +1,18 @@
+#/bin/bash
+
+source ./env.sh
+
+export ARCH
+
+for ARCH in $ARCHLIST
+do
+
+  for dir in gemm gemv
+  do
+    cd $dir
+    echo "==> $ARCH/$dir <=="
+    rocprofv3 --runtime-trace --marker-trace --pmc OccupancyPercent --output-format=pftrace -o amdrocblas_${dir}.$$.profile -- ./$ARCH/main.x T107L070.DAT | grep -v ' OK '  | grep -v '^[ ]*$' 
+    cd ..
+  done
+  
+done
